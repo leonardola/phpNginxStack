f7981a68cd5fff45d9c96436f1258794fe879d4f
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2017-12-02 18:31:39 -0200

    - added nginx configuration examplo for php 70 and 56 - added mysqli lib - changed the example page

---
+++ code/example/index.php
@@ -0,0 +1,12 @@
+<html style="text-align:center;">
+
+<h1>Welcome to the magento docker stack</h1> <br/>
+<?php
+echo "Php version is ". phpversion(). "<br/>";
+
+$link = mysqli_connect("db", "root", "root", "db") or die(mysqli_error());
+echo "Database connection is working<br/>";
+
+?>
+
+</html>
